23b0e3c8aa688b8be1282742fd39c98e3bdd869e
DebugJedi <priyankrao2512@gmail.com>
2024-10-09 15:43:43 -0400

    app test

--- objDetect_app.py
+++ objDetect_app.py
@@ -8,6 +8,6 @@ model = YOLO("src/best.pt")
 
 st.title("Real-Time Object Detection with YOLO")
 
-st.sidebar.title("Settings")
-confidence = st.sidebar.slider("Confidence Threshold", 0.0, 1.0, 0.8)
-webcam = st.sidebar.checkbox("Use Webcam", value=True)
+# st.sidebar.title("Settings")
+# confidence = st.sidebar.slider("Confidence Threshold", 0.0, 1.0, 0.8)
+# webcam = st.sidebar.checkbox("Use Webcam", value=True)
