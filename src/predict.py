bb129cbd31ac56e74e0d59b0b88ee0549350e4f9
DebugJedi <priyankrao2512@gmail.com>
2024-10-09 15:35:40 -0400

    app test

--- src/predict.py
+++ src/predict.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 
 
-def predict_stream(run=None):
+def predict_stream(run=False):
     while run:
         model = YOLO("src/best.pt")
         model.predict(source='1', show= True, save = False, conf= 0.8)
