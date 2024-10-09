03de919c48dc7578b496bfe698f3d2b54cf009bf
DebugJedi <priyankrao2512@gmail.com>
2024-10-09 15:45:26 -0400

    test app

--- objDetect_app.py
+++ objDetect_app.py
@@ -4,10 +4,21 @@ from ultralytics import YOLO
 import tempfile
 import numpy as np
 
-model = YOLO("src/best.pt")
+# model = YOLO("src/best.pt")
 
 st.title("Real-Time Object Detection with YOLO")
 
 # st.sidebar.title("Settings")
 # confidence = st.sidebar.slider("Confidence Threshold", 0.0, 1.0, 0.8)
 # webcam = st.sidebar.checkbox("Use Webcam", value=True)
+def test_webcam(index=0):
+    cap = cv2.VideoCapture(index)
+    if not cap.isOpened():
+        print(f"Cannot open webcam with index {index}")
+        return
+
+    print(f"Webcam {index} opened successfully.")
+    cap.release()
+
+if __name__ == "__main__":
+    test_webcam(0)
