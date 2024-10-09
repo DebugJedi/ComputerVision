cf7fc14b7b32a77a1d2f5226db3dba79c5a65d92
DebugJedi <priyankrao2512@gmail.com>
2024-10-09 13:51:04 -0400

    updated app

--- objDetect_app.py
+++ objDetect_app.py
@@ -1,30 +1,30 @@
 import streamlit as st
 import cv2
 from ultralytics import YOLO
+from src import predict
 
 def main():
     st.title("Webcam Object Detection")
-    
-    model = YOLO("src/best.pt")
-    
-    # Create a button to start/stop the webcam
-    if 'running' not in st.session_state:
-        st.session_state.running = False
+    model = predict.predict_stream()
+   
+    # # Create a button to start/stop the webcam
+    # if 'running' not in st.session_state:
+    #     st.session_state.running = False
 
-    if st.button('Start' if not st.session_state.running else 'Stop'):
-        st.session_state.running = not st.session_state.running
+    # if st.button('Start' if not st.session_state.running else 'Stop'):
+    #     st.session_state.running = not st.session_state.running
 
-    FRAME_WINDOW = st.image([])
-    camera = cv2.VideoCapture(1)
+    # FRAME_WINDOW = st.image([])
+    # camera = cv2.VideoCapture(1)
 
-    while st.session_state.running:
-        _, frame = camera.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = model.predict(frame, show=False, save=False, conf=0.8)
-        annotated_frame = results[0].plot()
-        FRAME_WINDOW.image(annotated_frame)
+    # while st.session_state.running:
+    #     _, frame = camera.read()
+    #     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    #     results = model.predict(frame, show=False, save=False, conf=0.8)
+    #     annotated_frame = results[0].plot()
+    #     FRAME_WINDOW.image(annotated_frame)
     
-    camera.release()
+    # camera.release()
 
 if __name__ == '__main__':
     main()
